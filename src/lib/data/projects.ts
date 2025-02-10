b7d4f72e222553909089663f5f04d95b5270504d
akump <kumpandrew@gmail.com>
2025-02-09 23:07:00 -0500

    Remove lorem

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -8,7 +8,7 @@ export const items: Array<Project> = [
 		color: '#8091F2',
 		description: 'Built a Chrome extensions for teams that provides various functionality',
 		shortDescription:
-			'Teams extension that had 25,000 users. It has since been taken down since the Microsoft Teams 2.0 release',
+			'Teams extension that had 25,000 users. It has since been taken down since the Microsoft Teams 2.0 release.',
 		links: [{ to: 'https://github.com/akump/MS-Teams-Always-Available', label: 'GitHub' }],
 		logo: Assets.Teams,
 		name: 'Microsoft Teams Chrome Extenion',
@@ -22,7 +22,7 @@ export const items: Array<Project> = [
 		description:
 			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
 		shortDescription:
-			'I created and most importantly maintaned a GroupMe dark theme until GroupMe created one themselves',
+			'I created and most importantly maintaned a GroupMe dark theme until GroupMe created one themselves.',
 		links: [{ to: 'https://github.com/akump/GroupMeDarkTheme', label: 'GitHub' }],
 		logo: Assets.GroupMe,
 		name: 'GroupMe Dark Theme',
@@ -35,7 +35,7 @@ export const items: Array<Project> = [
 		slug: 'andrews-recipes',
 		color: 'black',
 		description: 'Built this website in svelte and automated the adding of new recipes',
-		shortDescription: 'Simple website where my friends and family can easily find my recipes',
+		shortDescription: 'Simple website where my friends and family can easily find my recipes.',
 		links: [
 			{ to: 'https://github.com/akump/andrews-recipes', label: 'GitHub' },
 			{ to: 'https://andrews-recipes.surge.sh/', label: 'Link' }
--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -46,8 +46,7 @@ export const items = [
 	defineSkill({
 		slug: 'ts',
 		color: 'blue',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+		description: 'I have used Typescript on various interal tools and side projects',
 		logo: Assets.TypeScript,
 		name: 'Typescript',
 		category: 'pro-lang'
@@ -55,8 +54,7 @@ export const items = [
 	defineSkill({
 		slug: 'css',
 		color: 'blue',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+		description: '',
 		logo: Assets.CSS,
 		name: 'CSS',
 		category: 'markup-style'
@@ -64,8 +62,7 @@ export const items = [
 	defineSkill({
 		slug: 'html',
 		color: 'orange',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+		description: '',
 		logo: Assets.HTML,
 		name: 'HTML',
 		category: 'markup-style'
@@ -73,8 +70,7 @@ export const items = [
 	defineSkill({
 		slug: 'sass',
 		color: 'pink',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+		description: '',
 		logo: Assets.Sass,
 		name: 'Sass',
 		category: 'markup-style'
@@ -82,8 +78,7 @@ export const items = [
 	defineSkill({
 		slug: 'reactjs',
 		color: 'cyan',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+		description: '',
 		logo: Assets.ReactJs,
 		name: 'React Js',
 		category: 'library'
