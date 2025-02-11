7a31a183823dfed3d41547d9093740f44d038e9b
akump <kumpandrew@gmail.com>
2025-02-11 14:22:50 +0000

    Deploying to gh-pages from @ akump/andrew-website@a1b74e2c70b7714ccc6304fa1cfb6bf823a11ff1 🚀

--- _app/immutable/chunks/skills.03239b0d.js
+++ _app/immutable/chunks/skills.03239b0d.js
@@ -1,4 +1,4 @@
-import{A as o}from"./index.fc02c871.js";const u=`# Svelte
+import{A as o}from"./index.8ac6357f.js";const u=`# Svelte
 
 ---
 
@@ -10,7 +10,7 @@ The compiler itself is written in TypeScript. Its source code is licensed under 
 
 Svelte is :
 
-- compiled : Svelte shifts as much work as possible out of the browser and into your build step. No more manual optimisations — just faster, more efficient apps.
+- compiled : Svelte shifts as much work as possible out of the browser and into your build step. No more manual optimizations — just faster, more efficient apps.
 - compact : Write breathtakingly concise components using languages you already know — HTML, CSS and JavaScript. Oh, and your application bundles will be tiny as well.
 - complete : Built-in scoped styling, state management, motion primitives, form bindings and more — don't waste time trawling npm for the bare essentials. It's all here.
 
